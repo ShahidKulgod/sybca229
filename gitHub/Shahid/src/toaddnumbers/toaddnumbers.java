3440da11b229446e269a421573838d32b20dcae9
Shahid Kulgod <shahidkulgod96@gmail.com>
2024-01-18 10:32:17 +0530

    commit first

---
+++ gitHub/Shahid/src/toaddnumbers/toaddnumbers.java
@@ -0,0 +1,17 @@
+package toaddnumbers;
+
+public class toaddnumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int x=18;
+		int y=4;
+		int z=x+y;
+		int c=x-y;
+		System.out.println("The  Sum="+ z);
+		System.out.println("The  difference="+ c);
+	}
+
+}
